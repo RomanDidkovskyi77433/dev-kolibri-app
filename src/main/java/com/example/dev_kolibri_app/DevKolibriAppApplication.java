1f683984865158a454a057431c5a24ca2e603794
Roman <rdidkov1@stu.vistula.edu.pl>
2026-05-12 20:04:32 +0200

    Initial commit: basic Spring Boot app with /hello endpoint

---
+++ src/main/java/com/example/dev_kolibri_app/DevKolibriAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.dev_kolibri_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevKolibriAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevKolibriAppApplication.class, args);
+	}
+
+}
